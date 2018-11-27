41f3afae5620ff0b04635ff6cc31f98e74e31b1e
Robert Jackson <rjackson@linkedin.com>
2018-11-27 15:14:23 -0500

    Basic functionality.

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 'use strict';
 
-module.exports = function getFunctionLocation() {};
+const inspector = require('inspector');
+
+let uuid = 0;
+
+module.exports = function getFunctionLocation(needle) {
+  return new Promise(resolve => {
+    let scripts = {};
+    let globalPath = `__getFunctionLocation_${++uuid}`;
+    global[globalPath] = needle;
+
+    let session = new inspector.Session();
+
+    session.connect();
+
+    session.on('Debugger.scriptParsed', result => {
+      scripts[result.params.scriptId] = result;
+    });
+
+    session.post('Debugger.enable', () => {
+      session.post(
+        'Runtime.evaluate',
+        {
+          expression: `global.${globalPath}`,
+        },
+        (error, output) => {
+          session.post(
+            'Runtime.getProperties',
+            {
+              objectId: output.result.objectId,
+            },
+            (error, output) => {
+              let location = output.internalProperties.find(
+                prop => prop.name === '[[FunctionLocation]]'
+              );
+
+              let scriptForNode = scripts[location.value.value.scriptId];
+
+              resolve({
+                source: scriptForNode.params.url,
+                line: location.value.value.lineNumber + 1,
+                column: location.value.value.columnNumber + 1,
+              });
+            }
+          );
+        }
+      );
+    });
+  });
+};
--- tests/index-test.js
+++ tests/index-test.js
@@ -2,16 +2,40 @@
 
 const getFunctionLocation = require('../index');
 
-function foo() {}
+let foo = () => {};
+function bar() {}
+
+function buildFunction() {
+  return function() {};
+}
 
 QUnit.module('get-function-location', function() {
-  QUnit.test('generally works', async function(assert) {
+  QUnit.test('works for anonymous functions', async function(assert) {
+    let location = await getFunctionLocation(buildFunction());
+
+    assert.deepEqual(location, {
+      source: `file://${__filename}`,
+      line: 9,
+      column: 18,
+    });
+  });
+  QUnit.test('works for normal functions', async function(assert) {
+    let location = await getFunctionLocation(bar);
+
+    assert.deepEqual(location, {
+      source: `file://${__filename}`,
+      line: 6,
+      column: 13,
+    });
+  });
+
+  QUnit.test('works for arrow functions', async function(assert) {
     let location = await getFunctionLocation(foo);
 
     assert.deepEqual(location, {
-      source: __filename,
+      source: `file://${__filename}`,
       line: 5,
-      column: 0,
+      column: 11,
     });
   });
 });
