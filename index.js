1030e302978775ffcbbc60ea4486d67d4c7e71ca
Robert Jackson <rjackson@linkedin.com>
2018-11-27 15:37:30 -0500

    Refactor to leverage async/await.

--- index.js
+++ index.js
@@ -1,51 +1,41 @@
 'use strict';
 
 const inspector = require('inspector');
+const util = require('util');
 
 let uuid = 0;
 
-module.exports = function getFunctionLocation(needle) {
-  return new Promise(resolve => {
-    let scripts = {};
-    let globalPath = `__getFunctionLocation_${++uuid}`;
-    global[globalPath] = needle;
+module.exports = async function getFunctionLocation(needle) {
+  let scripts = {};
+  let globalPath = `__getFunctionLocation_${++uuid}`;
+  global[globalPath] = needle;
 
-    let session = new inspector.Session();
+  let session = new inspector.Session();
+  session.post = util.promisify(session.post);
 
-    session.connect();
+  session.connect();
 
-    session.on('Debugger.scriptParsed', result => {
-      scripts[result.params.scriptId] = result;
-    });
-
-    session.post('Debugger.enable', () => {
-      session.post(
-        'Runtime.evaluate',
-        {
-          expression: `global.${globalPath}`,
-        },
-        (error, output) => {
-          session.post(
-            'Runtime.getProperties',
-            {
-              objectId: output.result.objectId,
-            },
-            (error, output) => {
-              let location = output.internalProperties.find(
-                prop => prop.name === '[[FunctionLocation]]'
-              );
-
-              let scriptForNode = scripts[location.value.value.scriptId];
-
-              resolve({
-                source: scriptForNode.params.url,
-                line: location.value.value.lineNumber + 1,
-                column: location.value.value.columnNumber + 1,
-              });
-            }
-          );
-        }
-      );
-    });
+  session.on('Debugger.scriptParsed', result => {
+    scripts[result.params.scriptId] = result;
   });
+
+  await session.post('Debugger.enable');
+
+  let output = await session.post('Runtime.evaluate', {
+    expression: `global.${globalPath}`,
+  });
+
+  output = await session.post('Runtime.getProperties', {
+    objectId: output.result.objectId,
+  });
+
+  let location = output.internalProperties.find(prop => prop.name === '[[FunctionLocation]]');
+
+  let scriptForNode = scripts[location.value.value.scriptId];
+
+  return {
+    source: scriptForNode.params.url,
+    line: location.value.value.lineNumber + 1,
+    column: location.value.value.columnNumber + 1,
+  };
 };
