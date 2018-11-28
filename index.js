f2d42f56346e6ae5e533d9e3a203c53efe83e65c
Robert Jackson <rjackson@linkedin.com>
2018-11-28 10:32:44 -0500

    Avoid errors under Node 6.

--- index.js
+++ index.js
@@ -1,68 +1,96 @@
 'use strict';
 
-const inspector = require('inspector');
+const semver = require('semver');
 const util = require('util');
 
-let uuid = 0;
+// used to ensure that we only create a single session at a time
 let _sessionCompleted = Promise.resolve();
+let uuid = 0;
 
-module.exports = async function getFunctionLocation(needle) {
-  _sessionCompleted = _sessionCompleted.then(async function() {
+module.exports = function getFunctionLocation(needle) {
+  // node versions older than 8 do not support `inspector` module
+  // instead of failing "hard" here we resolve with `null`
+  if (semver.lt(process.version, '8.0.0')) {
+    return Promise.resolve(null);
+  }
 
+  /* eslint node/no-unsupported-features/node-builtins: ["error", { "version": ">= 8" } ] */
+
+  _sessionCompleted = _sessionCompleted.then(function() {
     // create a "unique" path so that we can easily reference this object on the global
     let globalPath = `__getFunctionLocation_${++uuid}`;
     global[globalPath] = needle;
 
+    const inspector = require('inspector');
+
     let session = new inspector.Session();
     session.post = util.promisify(session.post);
 
-    try {
-      session.connect();
+    let scripts = {};
 
-      let scripts = {};
-      session.on('Debugger.scriptParsed', result => {
-        scripts[result.params.scriptId] = result;
+    let ensureCleanup = () => {
+      return new Promise(resolve => {
+        // ensure that the RemoteObject that we created in `Runtime.evaluate` to
+        // use for grabbing the function location is properly released
+        resolve(
+          session.post('Runtime.releaseObjectGroup', {
+            objectGroup: globalPath,
+          })
+        );
+      }).then(() => {
+        session.disconnect();
+        delete global[globalPath];
       });
+    };
 
-      await session.post('Debugger.enable');
+    return Promise.resolve()
+      .then(() => {
+        session.connect();
 
-      // in order to get the function location we must have the internal objectId for the function
-      let output = await session.post('Runtime.evaluate', {
-        expression: `global.${globalPath}`,
-        objectGroup: globalPath,
-      });
+        session.on('Debugger.scriptParsed', result => {
+          scripts[result.params.scriptId] = result;
+        });
 
-      output = await session.post('Runtime.getProperties', {
-        objectId: output.result.objectId,
-      });
+        return session.post('Debugger.enable');
+      })
+      .then(() => {
+        // in order to get the function location we must have the internal objectId for the function
+        return session.post('Runtime.evaluate', {
+          expression: `global.${globalPath}`,
+          objectGroup: globalPath,
+        });
+      })
+      .then(output => {
+        return session.post('Runtime.getProperties', {
+          objectId: output.result.objectId,
+        });
+      })
+      .then(output => {
+        let location = output.internalProperties.find(prop => prop.name === '[[FunctionLocation]]');
 
-      let location = output.internalProperties.find(prop => prop.name === '[[FunctionLocation]]');
+        let scriptForNode = scripts[location.value.value.scriptId];
 
-      let scriptForNode = scripts[location.value.value.scriptId];
+        let source = scriptForNode.params.url;
 
-      let source = scriptForNode.params.url;
+        // Node 8 does not prefix the source with `file://` (but Node 10 and 11 do)
+        // this normalizes to ensure consistent result
+        if (!source.startsWith('file://')) {
+          source = 'file://' + source;
+        }
 
-      // Node 8 does not prefix the source with `file://` (but Node 10 and 11 do)
-      // this normalizes to ensure consistent result
-      if (!source.startsWith('file://')) {
-        source = 'file://' + source;
-      }
-
-      return {
-        source,
-        line: location.value.value.lineNumber + 1,
-        column: location.value.value.columnNumber + 1,
-      };
-    } finally {
-      // ensure that the RemoteObject that we created in `Runtime.evaluate` to
-      // use for grabbing the function location is properly released
-      await session.post('Runtime.releaseObjectGroup', {
-        objectGroup: globalPath,
-      });
-
-      session.disconnect();
-      delete global[globalPath];
-    }
+        return {
+          source,
+          line: location.value.value.lineNumber + 1,
+          column: location.value.value.columnNumber + 1,
+        };
+      })
+      .then(
+        value => ensureCleanup().then(() => value),
+        error =>
+          ensureCleanup().then(() => {
+            throw error;
+          })
+      );
   });
 
   return _sessionCompleted;
--- tests/index-test.js
+++ tests/index-test.js
@@ -9,6 +9,8 @@ function buildFunction() {
   return function() {};
 }
 
+const semver = require('semver');
+
 let GLOBAL_KEYS = Object.keys(global);
 QUnit.config.testTimeout = 5000;
 
@@ -17,53 +19,62 @@ QUnit.module('get-function-location', function(hooks) {
     assert.deepEqual(Object.keys(global), GLOBAL_KEYS);
   });
 
-  QUnit.test('works for anonymous functions', async function(assert) {
-    let location = await getFunctionLocation(buildFunction());
+  if (semver.lt(process.version, '8.0.0')) {
+    QUnit.module('when "inspector" module is not present', function() {
+      QUnit.test('does not fail / throw', function(assert) {
+        return getFunctionLocation(foo).then(result => {
+          assert.strictEqual(result, null);
+        });
+      });
+    });
 
-    assert.deepEqual(location, {
-      source: `file://${__filename}`,
-      line: 9,
-      column: 18,
+    // prevent remaining tests from running
+    return;
+  }
+
+  QUnit.test('works for anonymous functions', function(assert) {
+    return getFunctionLocation(buildFunction()).then(location => {
+      assert.deepEqual(location, {
+        source: `file://${__filename}`,
+        line: 9,
+        column: 18,
+      });
     });
   });
 
-  QUnit.test('works for normal functions', async function(assert) {
-    let location = await getFunctionLocation(bar);
-
-    assert.deepEqual(location, {
-      source: `file://${__filename}`,
-      line: 6,
-      column: 13,
+  QUnit.test('works for normal functions', function(assert) {
+    return getFunctionLocation(bar).then(location => {
+      assert.deepEqual(location, {
+        source: `file://${__filename}`,
+        line: 6,
+        column: 13,
+      });
     });
   });
 
-  QUnit.test('works for arrow functions', async function(assert) {
-    let location = await getFunctionLocation(foo);
-
-    assert.deepEqual(location, {
-      source: `file://${__filename}`,
-      line: 5,
-      column: 11,
+  QUnit.test('works for arrow functions', function(assert) {
+    return getFunctionLocation(foo).then(location => {
+      assert.deepEqual(location, {
+        source: `file://${__filename}`,
+        line: 5,
+        column: 11,
+      });
     });
   });
 
-  QUnit.test('handles concurrent requests without error', async function(assert) {
-    let first = getFunctionLocation(foo);
-    let second = getFunctionLocation(bar);
+  QUnit.test('handles concurrent requests without error', function(assert) {
+    return Promise.all([getFunctionLocation(foo), getFunctionLocation(bar)]).then(results => {
+      assert.deepEqual(results[0], {
+        source: `file://${__filename}`,
+        line: 5,
+        column: 11,
+      });
 
-    let firstResult = await first;
-    let secondResult = await second;
-
-    assert.deepEqual(firstResult, {
-      source: `file://${__filename}`,
-      line: 5,
-      column: 11,
-    });
-
-    assert.deepEqual(secondResult, {
-      source: `file://${__filename}`,
-      line: 6,
-      column: 13,
+      assert.deepEqual(results[1], {
+        source: `file://${__filename}`,
+        line: 6,
+        column: 13,
+      });
     });
   });
 });
